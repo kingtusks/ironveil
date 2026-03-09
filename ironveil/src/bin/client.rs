fea11f81b3cac45c1fa311af99f541dcb8cb5b92
tirth_laptop <tirth11804@gmail.com>
2026-03-09 19:58:18 -0400

    little setup for client.rs and server.rs before i move to developing on my server

--- ironveil/src/bin/client.rs
+++ ironveil/src/bin/client.rs
@@ -1,3 +1,17 @@
-fn main() {
+use rand::rngs::OsRng;
+use x25519_dalek::{PublicKey, StaticSecret};
+use base64::{Engine, engine::general_purpose::STANDARD};
+
+#[tokio::main]
+async fn main() {
+    let client_secret = StaticSecret::random_from_rng(OsRng);
+    let client_public = PublicKey::from(&client_secret);
+
+    //let encoded = STANDARD.encode(client_public.as_bytes());
+    //println!("{}", encoded);
+    //client pubkey: 7Mipe/LgN56gekMQlz6bJ3EQVQyoLnqd2Nal1UWNlg8=
     
+    let bytes = STANDARD.decode("YwNKYV92vswoJgf2Y3o84EMgkXQ8NQsDD859wqRSKns=").unwrap();
+    let server_public = PublicKey::from(<[u8; 32]>::try_from(bytes.as_slice()).unwrap());
+
 }
--- ironveil/src/bin/server.rs
+++ ironveil/src/bin/server.rs
@@ -1,3 +1,16 @@
-fn main() {
-    
+use boringtun::x25519::{StaticSecret, PublicKey};
+use rand::rngs::OsRng;
+use base64::{Engine, engine::general_purpose::STANDARD};
+
+#[tokio::main]
+async fn main() {
+    let server_secret = StaticSecret::random_from_rng(OsRng);
+    let server_public = PublicKey::from(&server_secret);
+
+    //let encoded = STANDARD.encode(server_public.as_bytes());
+    //println!("{}", encoded);
+    //server pubkey: YwNKYV92vswoJgf2Y3o84EMgkXQ8NQsDD859wqRSKns=
+
+    let bytes = STANDARD.decode("7Mipe/LgN56gekMQlz6bJ3EQVQyoLnqd2Nal1UWNlg8=").unwrap();
+    let client_public = PublicKey::from(<[u8; 32]>::try_from(bytes.as_slice()).unwrap());
 }
