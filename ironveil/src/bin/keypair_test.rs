678aad8d5e311897c30db20246fbb9a06eaf83ca
tirth_laptop <tirth11804@gmail.com>
2026-03-09 12:05:57 -0400

    confirmed crypto works

--- ironveil/src/bin/keypair_test.rs
+++ ironveil/src/bin/keypair_test.rs
@@ -1,4 +1,10 @@
+use x25519_dalek::{PublicKey, StaticSecret};
+use rand::rngs::OsRng;
+
 fn main() {
+    let secret = StaticSecret::random_from_rng(OsRng);
+    let public = PublicKey::from(&secret);
     
-    println!("testing");
+    println!("Private: {:02x?}", secret.to_bytes());
+    println!("Public: {:02x?}", public.as_bytes())
 }
