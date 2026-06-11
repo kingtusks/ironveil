1e5e45bfc6096edb7cc4b0bc0daf3c4b20653ea1
Tirth <tirth11804@gmail.com>
2026-06-11 08:17:11 -0400

    added dependencies for my backend and made db.rs to make sqlx easier

---
+++ backend/src/db.rs
@@ -0,0 +1,20 @@
+use dotenvy::dotenv;
+use sqlx::{PgPool, postgres::PgPoolOptions};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub pool: PgPool,
+    pub http: reqwest::Client,
+}
+
+pub async fn create_pool() -> AppState {
+    dotenv().ok();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&std::env::var("POSTGRES_URL").expect("postgres_url not set in .env"))
+        .await
+        .expect("failed to connect to postgres");
+
+    AppState {pool, http: reqwest::Client::new(),}
+}
