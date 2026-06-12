7b13b0fc23666c9d7f43a36bfa425028b050f027
Tirth <tirth11804@gmail.com>
2026-06-12 17:02:37 -0400

    did a bunch of random changes across the board

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,12 @@
-function App() {
-  
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import Home from './Home';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
+    </Router>
+  );
 }
