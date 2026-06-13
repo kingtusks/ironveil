0cd4f410b86cc0703bd0afc443305f239d3d3dd8
Tirth <tirth11804@gmail.com>
2026-06-12 23:08:14 -0400

    made a pretty shitty homepage (it just says home)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-//import Home from './Home';
+import Home from './Home';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
